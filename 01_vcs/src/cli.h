1c5fcaff7a1afd4433dbc9ac3124462c71f0b50b
Dego1999 <matvey92225@gmail.com>
2023-02-27 18:59:45 +0300

    (feat:New_f)

--- 01_vcs/src/cli.h
+++ 01_vcs/src/cli.h
@@ -8,5 +8,6 @@ bool handle_init_command(const std::vector<std::string>& args);
 bool handle_commit_command(const std::vector<std::string>& args);
 bool handle_diff_command(const std::vector<std::string>& args);
 bool handle_log_command(const std::vector<std::string>& args);
+bool handle_label_command(const std::vector<std::string>& args);
 
 #endif
