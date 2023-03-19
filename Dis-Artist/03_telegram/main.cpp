4874bfa58ef088314c76c4f1cec40376c29197ca
bogdanova21 <113133147+bogdanova21@users.noreply.github.com>
2023-03-19 19:40:03 +0300

    feat(03_telegram): upd main.cpp

--- Dis-Artist/03_telegram/main.cpp
+++ Dis-Artist/03_telegram/main.cpp
@@ -1,47 +1,89 @@
-#include <tgbot/tgbot.h>
+#include <boost/json/src.hpp>
+#include <boost/locale.hpp>
+#include <boost/program_options.hpp>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
+#include "fmt/core.h"
 #include "config.h"
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        std::cerr << "Error! Missing config file path" << std::endl;
+#ifndef TGBOT_ENABLE_INLINE_KEYBOARD
+#include "bot/bot.hpp"
+#else
+#include "bot/bot_inline_keyboard/bot_inline_keyboard.hpp"
+#endif
+
+#include "course/course.hpp"
+#include "tgbot/tgbot.h"
+#include "weather/weather.hpp"
+
+using namespace std;
+using namespace TgBot;
+
+int main(int argc, char* argv[]) {
+    // Bot setup
+    boost::locale::generator gen;
+    locale::global(gen(""));
+
+    string bot_token_str;
+    string weather_api_str;
+
+    try {
+        boost::program_options::options_description desc("Allowed Options");
+        boost::program_options::variables_map vm;
+
+        desc.add_options()("help,h", "Get allowed options")(
+            "token,t", boost::program_options::value<string>()->required(),
+            "Set path to bot_token.txt")(
+            "api,a", boost::program_options::value<string>()->required(),
+            "Set path to openweathermap_api.txt");
+
+        boost::program_options::store(
+            boost::program_options::parse_command_line(argc, argv, desc), vm);
+
+        if (vm.count("help")) {
+            cout << "Usage: tgbot [options...]\n\n" << desc;
+            return 1;
+        }
+
+        if (vm.count("token")) {
+            ifstream bot_token_file(vm["token"].as<string>());
+            if (bot_token_file.is_open()) {
+                bot_token_file >> bot_token_str;
+            } else {
+                fmt::print("Error: while opening file {}: {}\n", vm["token"].as<string>(), strerror(errno));
+                return 1;
+            }
+        }
+
+        if (vm.count("api")) {
+            ifstream weather_api_file(vm["api"].as<string>());
+            if (weather_api_file.is_open()) {
+                weather_api_file >> weather_api_str;
+            } else {
+                fmt::print("Error: while opening file {}: {}\n", vm["api"].as<string>(), strerror(errno));
+                return 1;
+            }
+        }
+
+        boost::program_options::notify(vm);
+    } catch (const exception& ex) {
+        cout << "Error: " << ex.what() << '\n';
         return 1;
     }
 
-    try
-    {
-        const auto token = config::get_token(argv[1]);
-        auto bot = TgBot::Bot(token);
+#ifndef TGBOT_ENABLE_INLINE_KEYBOARD
+    bot bot(bot_token_str, weather_api_str);
+#else
+    bot_inline_keyboard bot(bot_token_str, weather_api_str);
+#endif
 
-        // Bot config
-        bot.getEvents().onCommand(
-            "start", [&bot](TgBot::Message::Ptr message)
-            { bot.getApi().sendMessage(message->chat->id, "Hi!"); });
-
-        bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
-                                     {
-            std::cout << ">> " << message->text.c_str() << std::endl;
-            if (StringTools::startsWith(message->text, "/start")) {
-                return;
-            }
-            bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text); });
-
-        // Start
-        const auto bot_name =  bot.getApi().getMe()->username.c_str();
-        std::cout << "Bot name: " << bot_name << std::endl;
-        TgBot::TgLongPoll longPoll(bot);
-        while (true)
-        {
-            std::cout << "Long poll..." << std::endl;
-            longPoll.start();
-        }
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error! " << e.what() << '\n';
+    try {
+        bot.start();
+    } catch (TgBot::TgException& ex) {
+        cout << "Error: " << ex.what() << '\n';
     }
 
     return 0;
