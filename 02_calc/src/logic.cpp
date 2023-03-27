1285689045d9cdfde0f0efce193a2a049ef079cb
2022-polytech-%yudin% <113236987+M1XANCHOS@users.noreply.github.com>
2023-03-27 18:24:09 +0300

    Update logic.cpp

--- 02_calc/src/logic.cpp
+++ 02_calc/src/logic.cpp
@@ -1,8 +1,12 @@
 #include "logic.h"
+#include <cmath>
 #include <sstream>
+#include <vector>
 
 namespace calc
 {
+    std::vector<std::string> history;   // история операций
+
     std::string Logic::process_math()
     {
         if (expression.empty())
@@ -14,18 +18,85 @@ namespace calc
         std::stringstream output_ss;
 
         int a;
-        int b;
         char op;
-        input_ss >> a >> op >> b;
+        input_ss >> a >> op;
 
-        if (op == '+')
+        switch (op)
         {
-            output_ss << a << op << b << "=" << a + b;
+            case '+':
+                {
+                    int b;
+                    input_ss >> b;
+                    output_ss << a << op << b << "=" << a + b;
+                    break;
+                }
+            case '-':
+                {
+                    int b;
+                    input_ss >> b;
+                    output_ss << a << op << b << "=" << a - b;
+                    break;
+                }
+            case '*':
+                {
+                    int b;
+                    input_ss >> b;
+                    output_ss << a << op << b << "=" << a * b;
+                    break;
+                }    
+            case '/':
+                {
+                    int b;
+                    input_ss >> b;
+                    output_ss << a << op << b << "=" << a / b;
+                    break;
+                }
+            case '^':
+                {
+                    if (input_ss.peek() == '2')
+                    {
+                        input_ss.ignore(); // пропускаем символ '^'
+
+                        output_ss << a << "^2" << "=" << pow(a, 2);
+                    }
+                    else
+                    {
+                        output_ss << "Неизвестная операция";
+                    }
+                    break;
+                }
+            case 's':
+                {
+                    if (input_ss.peek() == 'q')
+                    {
+                        input_ss.ignore(4); // пропускаем символы 's', 'q', 'r', 't'
+
+                        output_ss << "sqrt(" << a << ")" << "=" << sqrt(a);
+                    }
+                    else
+                    {
+                        output_ss << "Неизвестная операция";
+                    }
+                    break;
+                }
+            default:
+                {
+                    output_ss << "Неизвестная операция";
+                    break;
+                }
         }
-        else
-        {
-            output_ss << a << op << b << "=" << a + b;
-        }
+
+        history.push_back(output_ss.str());    // добавляем операцию в историю
         return output_ss.str();
     }
+
+    void Logic::print_history()
+    {
+        std::cout << "История операций:\n";
+        for (auto op : history)
+        {
+            std::cout << op << '\n';
+        }
+    }
+}
 }
