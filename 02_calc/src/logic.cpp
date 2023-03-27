5f7173477b0a2fda5f27ba9e875109f2acb778ad
AP04D <mrandpan2004@mail.ru>
2023-03-27 20:27:26 +0300

    feat: added const operations

--- 02_calc/src/logic.cpp
+++ 02_calc/src/logic.cpp
@@ -13,8 +13,8 @@ namespace calc
         std::istringstream input_ss(expression);
         std::stringstream output_ss;
 
-        int a;
-        int b;
+        double a;
+        double b;
         char op;
         input_ss >> a >> op >> b;
 
@@ -22,10 +22,66 @@ namespace calc
         {
             output_ss << a << op << b << "=" << a + b;
         }
+        else if (op == '-')
+        {
+            output_ss << a << op << b << "=" << a - b;
+        }
+        else if (op == '*')
+        {
+            output_ss << a << op << b << "=" << a * b;
+        }
+        else if (op == '/')
+        {
+            if (b == 0)
+            {
+                output_ss << "Error: division by zero";
+            }
+            else
+            {
+                output_ss << a << op << b << "=" << a / b;
+            }
+        }
+        else if (op == '^')
+        {
+            output_ss << a << op << b << "=" << pow(a, b);
+        }
+        else if (op == '!')
+        {
+            if (a < 0)
+            {
+                output_ss << "Error: factorial of a negative number";
+            }
+            else
+            {
+                int result = 1;
+                for (int i = 1; i <= a; ++i)
+                {
+                    result *= i;
+                }
+                output_ss << a << op << "=" << result;
+            }
+        }
+        else if (op == 'p')
+        {
+            output_ss << "pi=" << pi;
+        }
+        else if (op == 'e')
+        {
+            output_ss << "e=" << exp(1.0);
+        }
+        else if (op == 'c')
+        {
+            output_ss << "c=" << 299792458;
+        }
+        else if (op == 'G')
+        {
+            output_ss << "G=" << 6.67430e-11;
+        }
         else
         {
-            output_ss << a << op << b << "=" << a + b;
+            output_ss << "Error: unsupported operation or constant";
         }
         return output_ss.str();
+ 
     }
 }
