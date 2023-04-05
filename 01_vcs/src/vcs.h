12c99ecdfe1f6b2f885413e0d1a7386e8d2aea8d
Constantine Korikov <constantine.korikov@gmail.com>
2023-04-05 22:28:42 +0800

    fix(repo): revert

---
+++ 01_vcs/src/vcs.h
@@ -0,0 +1,13 @@
+#ifndef VCS_H
+#define VCS_H
+
+#include <string>
+#include <unordered_map>
+
+bool vcs_init();
+bool vcs_commit(const std::string& message);
+bool vcs_log();
+
+std::unordered_map<std::string, std::string> create_snapshot();
+
+#endif
