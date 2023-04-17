3daa9b0640cc1913dda269048bc0c18b128d3a61
pavelskiba <pavel.v.skiba@gmail.com>
2023-04-17 20:27:32 +0300

    skiba f2

--- CheeseGang/02_calc/src/logic.cpp
+++ CheeseGang/02_calc/src/logic.cpp
@@ -1,5 +1,6 @@
 #include "logic.h"
 #include <sstream>
+#include <unordered_map>
 
 namespace calc
 {
@@ -9,6 +10,8 @@ namespace calc
         {
             return "";
         }
+  
+        std::unordered_map<char, double> variables;
 
         std::istringstream input_ss(expression);
         std::stringstream output_ss;
@@ -18,81 +21,97 @@ namespace calc
         char op;
         input_ss >> a >> op >> b;
 
-        if (op == '+')
+        if (op == '=')
         {
-            output_ss << a << op << b << "=" << a + b;
-        }
-        else if (op == '-')
-        {
-            output_ss << a << op << b << "=" << a - b;
-        }
-        else if (op == '*')
-        {
-            output_ss << a << op << b << "=" << a * b;
-        }
-        else if (op == '/')
-        {
-            if (b == 0)
-            {
-                output_ss << "Error: division by zero";
-            }
-            else
-            {
-                output_ss << a << op << b << "=" << a / b;
-            }
-        }
-        else if (op == '%')
-        {
-            if (b == 0)
-            {
-                output_ss << "Error: division by zero";
-            }
-            else
-            {
-                output_ss << a << op << b << "=" << a % b;
-            }
-        }
-        else if (op == '^')
-        {
-            output_ss << a << op << b << "=" << pow(a, b);
-        }
-        else if (op == '!')
-        {
-            if (a < 0)
-            {
-                output_ss << "Error: factorial of a negative number";
-            }
-            else
-            {
-                int result = 1;
-                for (int i = 1; i <= a; ++i)
-                {
-                    result *= i;
-                }
-                output_ss << a << op << "=" << result;
-            }
-        }
-        else if (op == 'p')
-        {
-            output_ss << "pi=" << pi;
-        }
-        else if (op == 'e')
-        {
-            output_ss << "e=" << exp(1.0);
-        }
-        else if (op == 'c')
-        {
-            output_ss << "c=" << 299792458;
-        }
-        else if (op == 'G')
-        {
-            output_ss << "G=" << 6.67430e-11;
+           variables[a] = b;
+            output_ss << a << op << b;
         }
         else
         {
-            output_ss << "Error: unsupported operation or constant";
+            if (variables.count(a))
+            {
+                a = variables[a];
+            }
+            if (variables.count(b))
+            {
+                b = variables[b];
+            }
+
+            if (op == '+')
+            {
+                output_ss << a << op << b << "=" << a + b;
+            }
+            else if (op == '-')
+            {
+                output_ss << a << op << b << "=" << a - b;
+            }
+            else if (op == '*')
+            {
+                output_ss << a << op << b << "=" << a * b;
+            }
+            else if (op == '/')
+            {
+                if (b == 0)
+                {
+                    output_ss << "Error: division by zero";
+                }
+                else
+                {
+                    output_ss << a << op << b << "=" << a / b;
+                }
+            }
+            else if (op == '%')
+            {
+                if (b == 0)
+                {
+                    output_ss << "Error: division by zero";
+                }
+                else
+                {
+                    output_ss << a << op << b << "=" << static_cast<int>(a) % static_cast<int>(b);
+                }
+            }
+            else if (op == '^')
+            {
+                output_ss << a << op << b << "=" << pow(a, b);
+            }
+            else if (op == '!')
+            {
+                if (a < 0)
+                {
+                    output_ss << "Error: factorial of a negative number";
+                }
+                else
+                {
+                    int result = 1;
+                    for (int i = 1; i <= a; ++i)
+                    {
+                        result *= i;
+                    }
+                    output_ss << a << op << "=" << result;
+                }
+            }
+            else if (op == 'p')
+            {
+                output_ss << "pi=" << pi;
+            }
+            else if (op == 'e')
+            {
+                output_ss << "e=" << exp(1.0);
+            }
+            else if (op == 'c')
+            {
+                output_ss << "c=" << 299792458;
+            }
+            else if (op == 'G')
+            {
+                output_ss << "G=" << 6.67430e-11;
+            }
+            else
+            {
+                output_ss << "Error: unsupported operation or constant";
+            }
         }
         return output_ss.str();
- 
     }
 }
