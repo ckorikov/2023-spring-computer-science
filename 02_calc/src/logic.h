66fc8072bc825768e4687b582f71953b5d6d71f9
2022-polytech-%yudin% <113236987+M1XANCHOS@users.noreply.github.com>
2023-04-24 15:35:47 +0300

    Update logic.h

--- 02_calc/src/logic.h
+++ 02_calc/src/logic.h
@@ -2,6 +2,7 @@
 #define LOGIC_HPP
 
 #include <string> // for char_traits, operator+, string, basic_string
+#include <unordered_map> // added for variable support
 
 namespace calc
 {
@@ -10,8 +11,12 @@ namespace calc
     {
         std::string process_math();
         std::string expression;
+
+        // added for variable support
+        std::unordered_map<std::string, int> variables;
     };
 
 }
 
+#endif // LOGIC_HPP
 #endif // LOGIC_HPP
